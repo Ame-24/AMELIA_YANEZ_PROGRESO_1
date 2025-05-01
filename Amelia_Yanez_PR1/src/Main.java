2f993fe1573f39d374c4a4a42434df1176b743c0
Ame-24 <salome24ys2005@gmail.com>
2025-05-01 17:25:42 -0500

    Main.java

--- Amelia_Yanez_PR1/src/Main.java
+++ Amelia_Yanez_PR1/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         TiendaMascotas tienda = new TiendaMascotas();
 
+        int opcion = 0;
+
 
         tienda.agregarEspecie(new Especie("Cacatúa Blanca", 40, "Aves", 1500.0, 10));
         tienda.agregarEspecie(new Especie("Boa Constrictor", 80, "Reptil", 1800.0, 20));
@@ -14,6 +16,37 @@ public class Main {
         tienda.mostrarEspecies();
 
         System.out.println("Venta:");
-        tienda.mostrarEspecies();
+        System.out.println(tienda.venderMascota("Cacatúa Blanca", 4));
 
+        while (opcion != 6) {
+            System.out.println("\n--- Tienda Mascotas ---");
+            System.out.println("1. Ver información de especies");
+            System.out.println("2. Vender especies");
+            System.out.println("3. Comprar especies");
+            System.out.println("4. Buscar especies");
+            System.out.println("5. Buscar especie menos vendida");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        }
+        sc.close();
     }
+}
